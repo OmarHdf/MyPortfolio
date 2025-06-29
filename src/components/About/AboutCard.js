0b321ab37458998a5db77c367a5ddb5e3d321133
Omar El Hedfi <73970289+OmarHdf@users.noreply.github.com>
2025-06-29 16:01:21 +0200

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,33 +8,33 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Bonjour à tous, je suis <span className="purple">Omar El Hedfi</span>  
+            de <span className="purple">Kairouan, Tunisie</span>.
             <br />
-            I am currently employed as a software developer at Juspay.
+            Actuellement diplômé en Technologies de l'Information de l'ISET Kairouan,  
+            je suis passionné par le développement logiciel, l'administration système et les pratiques DevOps.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            Je travaille sur des projets intégrant l'intégration continue, le déploiement continu et la sécurité (DevSecOps).
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            En dehors du codage, voici quelques activités que j'aime faire :
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Participer à des projets open source
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Lire sur l’intelligence artificielle et le DevOps
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Écrire des scripts d'automatisation
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Construis des solutions qui ont un impact concret et durable !"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Omar El Hedfi</footer>
         </blockquote>
       </Card.Body>
     </Card>
@@ -42,3 +42,4 @@ function AboutCard() {
 }
 
 export default AboutCard;
+
